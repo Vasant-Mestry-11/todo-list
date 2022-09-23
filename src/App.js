e7e788ee201141ce8255f727ff21a3208308f23f
Vasant-Mestry-11 <vasantmestry07@gmail.com>
2022-09-23 16:22:43 +0530

    Created Todo List with webpack setup

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Todos from "./components/Todos";
+
+export function App() {
+  return (
+    <div className="todo-container">
+      <Todos />
+    </div>
+  );
+}
